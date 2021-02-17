d21c65a48adccf63b0425e6b6370666920daa567
Prasad_Dangare <67819815+PRASAD-DANGARE@users.noreply.github.com>
2021-02-17 21:42:13 +0530

    Find The Length Of The Given String From User

---
+++ Assignment1_10.py
@@ -0,0 +1,16 @@
+'''
+
+Function Name : main()
+Description   : From main Function Get The String And Find The Length Of The Given String
+Author        : Prasad Vijaykumar Dangare
+Date          : 17-02-2021 
+
+'''
+
+def main():
+    
+    Str_value = (input("Enter Your Name : ")) # By Default Every Thing Is Consider As String So Str_Value Is Taken As Str
+    print("Length Of Given Name Is ", len(Str_value)) # len is use for to get the length 
+
+if __name__ == "__main__":
+    main()
