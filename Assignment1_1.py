2b375d54e50cc26062ce12788ef93b8d9835a75f
Prasad_Dangare <67819815+PRASAD-DANGARE@users.noreply.github.com>
2021-02-17 21:33:27 +0530

    Calling The Fun Function From Main

---
+++ Assignment1_1.py
@@ -0,0 +1,19 @@
+'''
+
+Function Name : Fun()
+Description   : Calling The Fun Function From Main
+Author        : Prasad Vijaykumar Dangare
+Date          : 17-02-2021 
+
+'''
+
+def Fun():
+    print("Hello Inside Fun") # Message Inside Fun Function
+
+def main():
+
+    Fun() # Calling The Function Fun
+
+if __name__ == "__main__":
+    main()
+
