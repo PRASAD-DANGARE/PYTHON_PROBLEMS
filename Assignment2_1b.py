74bfdd91a47d41eab25fa3c31a632eefe1bfcbb0
Prasad_Dangare <67819815+PRASAD-DANGARE@users.noreply.github.com>
2021-02-18 23:51:44 +0530

    It Contain Function For Assignment2_1a.py
    This Module Get The Function Call From Assignment2_1a.py

---
+++ Assignment2_1b.py
@@ -0,0 +1,28 @@
+'''
+
+Function Name : Add, Sub, Mult, Div()
+Description   : This Module Get The Function Call From Assignment2_1a.py 
+Author        : Prasad Vijaykumar Dangare
+Date          : 18-02-2021 
+
+'''
+
+def Add(iNum1, iNum2):
+    iRet = 0
+    iRet = iNum1 + iNum2
+    return iRet
+
+def Sub(iNum1, iNum2):
+    iRet = 0
+    iRet = iNum1 - iNum2
+    return iRet
+
+def Mult(iNum1, iNum2):
+    iRet = 0
+    iRet = iNum1 * iNum2
+    return iRet
+
+def Div(iNum1, iNum2):
+    iRet = 0
+    iRet = iNum1 / iNum2
+    return iRet
