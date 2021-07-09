3fcadc5981be49ecbe87808626a4bccd2bd09e44
Prasad_Dangare <67819815+PRASAD-DANGARE@users.noreply.github.com>
2021-07-09 19:51:01 +0530

    Accept Number From User And Return Largest Number

---
+++ Problems_On_Digits/Program11.py
@@ -0,0 +1,55 @@
+'''
+Function Name    :  Max_Digit
+Description      :  Accept Number From User And Return Largest Number
+Function Date    :  09 July 2021
+Function Author  :  Prasad Dangare
+Input            :  Int
+Output           :  Int
+
+'''
+
+#============================
+#
+# Max_Digit Operation
+#
+#============================
+
+def Max_Digit(iValue1):
+
+    iDigit = 0
+    iMax = 0
+
+    if iValue1 < 0:
+        iValue1 =- iValue1
+
+    while iValue1 > 0:
+
+        iDigit = iValue1 % 10
+        if iDigit > iMax:
+            iMax = iDigit
+        iValue1 = iValue1 // 10
+
+    return iMax
+
+#====================
+#
+# Entry Point 
+#
+#====================
+
+def main():
+
+    iNo1 = int(input("Enter The Number : "))
+
+    iRet = Max_Digit(iNo1)
+
+    print("Largest Digit is : ", iRet)
+
+#====================
+#
+# Code Starter
+#
+#====================
+
+if __name__ == "__main__":
+    main()
