1dee0adc992229d14128ff855eedcac91736492b
Prasad_Dangare <67819815+PRASAD-DANGARE@users.noreply.github.com>
2021-07-10 15:37:01 +0530

    Accept Number From User And Display * # On Screen

---
+++ Problems_On_Pattern-Printing/Program2.py
@@ -0,0 +1,49 @@
+'''
+Function Name    :  Pattern
+Description      :  Accept Number From User And Display * # On Screen Using Linear Pattern Printing
+Function Date    :  10 July 2021
+Function Author  :  Prasad Dangare
+Input            :  Int
+Output           :  Special Simbol
+
+'''
+
+#============================
+#
+# Pattern Operation
+#
+#============================
+
+def Pattern(iValue1):
+
+    print("\n")
+    for _ in range(1, iValue1 + 1):
+        print(" ", end = "*\t#\t")
+    print("\n")
+
+#====================
+#
+# Entry Point 
+#
+#====================
+
+def main():
+
+    iNo1 = int(input("Enter The Number : "))
+
+    Pattern(iNo1)
+
+#====================
+#
+# Code Starter
+#
+#====================
+
+if __name__ == "__main__":
+    main()
+
+'''
+Input  : 5
+Output : *      #        *      #        *      #        *      #        *      #
+
+'''
