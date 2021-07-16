66e334e0494252c3d6abe608e588323b4515cd98
Prasad_Dangare <67819815+PRASAD-DANGARE@users.noreply.github.com>
2021-07-16 20:44:02 +0530

    Accept String & Display The String On Screen

---
+++ Problems_On_Strings/Program1.py
@@ -0,0 +1,31 @@
+'''
+Function Name    :  main
+Description      :  Accept String & Display The String On Screen
+Function Date    :  16 July 2021
+Function Author  :  Prasad Dangare
+Input            :  str
+Output           :  str
+
+'''
+
+#====================
+#
+# Entry Point 
+#
+#====================
+
+def main():
+
+    str_value = input("Enter Your Name : ")
+
+    print("Your Name Is : ", str_value)
+    print("Length Of This String is : ", len(str_value))
+
+#====================
+#
+# Code Starter
+#
+#====================
+
+if __name__ == "__main__":
+    main()
